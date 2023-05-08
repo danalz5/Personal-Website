4fea288542ce6338a765943a011d692ea5a55383
danalz5 <danielalzeidan@gmail.com>
2023-05-08 01:28:41 -0500

    div hover animation

--- portfolio/src/components/LinksBox.js
+++ portfolio/src/components/LinksBox.js
@@ -4,15 +4,15 @@ import github from "../images/github.png"
 import email from "../images/email.png"
 
 function LinksBox() {
-    return (
-      <div className="LinksBox">
-        <ul>
-          <li> <a href="https://www.linkedin.com/in/daniel-alzeidan/" target="_blank" rel="noopener noreferrer"> <img src={linkedin} alt="LinkedIn"/> </a> </li>
-          <li> <a href="https://github.com/danalz5" target="_blank" rel="noopener noreferrer"> <img src={github} alt="GitHub"/> </a> </li>
-          <li> <a href="mailto:danielalzeidan@gmail.com" target="_blank" rel="noopener noreferrer"> <img src={email} alt="Email"/> </a> </li>
-        </ul>
-      </div>
-    );
-  }
-  
-  export default LinksBox;
+  return (
+    <div className="LinksBox">
+      <ul>
+        <li> <a href="https://www.linkedin.com/in/daniel-alzeidan/" target="_blank" rel="noopener noreferrer"> <img className="icon" src={linkedin} alt="LinkedIn"/> </a> </li>
+        <li> <a href="https://github.com/danalz5" target="_blank" rel="noopener noreferrer"> <img className="icon" src={github} alt="GitHub"/> </a> </li>
+        <li> <a href="mailto:danielalzeidan@gmail.com" target="_blank" rel="noopener noreferrer"> <img className="icon" src={email} alt="Email"/> </a> </li>
+      </ul>
+    </div>
+  );
+}
+
+export default LinksBox;
