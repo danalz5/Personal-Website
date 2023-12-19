7782cd183153ed93877ab818c216bea3dcc333bf
danalz5 <danielalzeidan@gmail.com>
2023-12-18 18:03:43 -0600

    info updates

--- portfolio/src/util/createInfoArray.js
+++ portfolio/src/util/createInfoArray.js
@@ -2,17 +2,19 @@
 // returns an Array full of html, each element is a different set of info
 // to be displayed in the InfoArea
 function createInfoArray() {
+    const linkStyle = {
+        color: 'white',
+    };
     const InfoArray = [];
     InfoArray.push(
         <>
             <h1>About Me</h1>
             <p>
-                Hi, my name is Daniel Alzeidan! I'm currently working towards a B.S in Computer Science
-                at the University of Illinois-Chicago and expect to graduate after the Fall 2023 semester.
-                Right now I'm looking for an internship or entry level job as a software/web devleoper. I'm trying to teach
-                myself UI/UX because I enjoy the creativity that comes with working on the front-end but eventually I want
-                to learn enough to be able to consider myself a full stack developer. Teaching myself the basics of front-end 
-                is one of the reasons I made this website!
+                Hi, my name is Daniel Alzeidan! I'm a graduate from the University of Illinois - Chicago with a B.S. in Computer Science.
+                Right now I'm looking for a job in the software engineering/development field. I'm also trying to
+                teach myself new tools and technologies while I apply for jobs.
+                Right now I'm working towards getting AWS certifications and teaching myself the basics of front-end 
+                which is one of the reasons I made this website!
                 <br/><br/>
                 If you want to learn more about me or see projects that I've worked on click the arrows!
             </p> 
@@ -23,15 +25,12 @@ function createInfoArray() {
         <>
             <h1>Skills</h1>
             <p>
-                Programming: C/C++, Java, Python, HTML/CSS/JS
+                Programming: C/C++, Java, Python, HTML/CSS/JS, SQL
+                <br/>
+                Tools/Technologies: React, matplotlib/seaborn, VSCode, pycharm, numpy, pandas
                 <br/><br/>
-                For C/C++, Java, and Python my experience comes mostly from class.
-                I first learned to program my freshman year of college in basic C and then
-                learned Data Structures in C++. My experience in Java comes from a Software Design class
-                I took that emphasized OOP. I did a couple projects in that class like making a chat room and 
-                making the lottery game KENO. These projects also utilized JavaFX for the UI. My experience in
-                Python comes from a class I took my sophomore year that taught us basic webscraping, data science
-                tools like 'numpy' and 'pandas', and database tools like SQL. Lastly my knowledge in HTML/CSS/JSS is all self taught.
+                Most of my programming knowledge comes from classes I took in college. HTML/CSS/JS and React knowledge come from
+                from putting this website together. I've also done a couple of class projects with them too.
                 I created this website partly to teach myself the basics of these languages and also the basics of React.js.
                 <br/><br/>
                 Soft Skills: <br/>
@@ -51,45 +50,54 @@ function createInfoArray() {
                 students could use to track air quality readings in different buildings around campus and other
                 popular areas near campus.
                 <br/><br/>
-                For this project me and one person in my group focused on front end while the other two members focused on
-                the back end. For the project we used tools like React and Firebase. A big focus of the class was on employing
-                different agile methodologies during devleopment. Our group utilized two different methodologies, Kanban and Scrum.
-                We used the idea of a Work-In-Progress board heavily throughout development. Since each of us was still a student at
-                the end of the day we had pretty conflicting schedules to we couldn't talk to each other every single day, so the WIP
-                board helped us out alot. It was really useful in keeping track of what was being done and by who. As for scrum, we utilized
-                the idea of sprints. It was a good way for us to plan the roadmap of development over the 12 weeks we worked on this project.
+                For this project we were tasked with using Agile Methodologies throughout the semester when developing our application.
+                Our group used Kanban and Scrum across the 12 week development process.
                 <br/><br/>
                 My contributions were mostly on the front end. My job was to focus on the map feature and to also make sure our UI looked
                 clean and presentable. To create the map feature I used the React-Leaflet library which is a version of leaflet that
                 takes advantage of React components. I made it so the map was populated with different markers that when clicked on
                 would display air quality readings pulled from our database. One of the features on the map I also implemented were
                 circles that would highlight areas where air quality readings were at unhealthy levels.
-                <br/><br/>
-                This project isn't on my github yet, but I'm planning on pushing it soon. I'm also planning on adding features myself that
-                we didn't get to in the original version of the project. One of my ideas is to create a "News" page that pulls information from
-                different sources to show things like severe weather warnings, tornado warnings, etc.
             </p>
+        </>
+    );
+    
+    InfoArray.push(
+        <>
+            <h1>Project 2 : Connect Food</h1>
+            <p>
+                This is a group project I did for my User Interface Design class. This website was made without the use of a framework.
+                Our task was to solve a user experience problem so our group's idea was to solve how difficult it is for people, especially those
+                with dietary restrictions, to find dishes they can eat. The concept of the app is that you would add friends who could upload recommendations
+                with certain criteria as well as recipes. Since this was a UI Design class we were told not to implement backend.
+                My main job was the homepage, the navbar, the profile page, and making sure styling was consistent across all pages. 
+                The profile page can be edited and preferences can be set and that determines which dishes can be recommended to the user. For example if a user sets
+                that they are vegetarian they would only be shown recommendations that are vegetarian. I also got to play around with and learn about the localStorage
+                API to make sure information on the website was consistent across each page and persisted between uses. If you want to play around on the webpage there
+                is a link below!
+            </p>
+            <a href="https://connect-food.vercel.app" style={linkStyle} target="_blank" rel="noopener noreferrer">
+                https://connect-food.vercel.app
+            </a>
         </>
     );
 
     InfoArray.push(
         <>
-            <h1>Project 2 : Student Management Application</h1>
+            <h1>Project 3 : Student Management Application</h1>
             <p>
                 This was a project that I did for the Python class I took. The application allowed users to
                 input student information and have it stored in a database. After adding students to the system
                 the user could then lookup the inputted information such as student ID number or student email.
                 The project is pretty simple but it was good for learning how to use SQL and making a simple
                 python application.
-                <br/><br/>
-                This project also isn't on github yet but will be soon.
             </p>
         </>
     );
 
     InfoArray.push(
         <>
-            <h1>Project 3 : This Website</h1>
+            <h1>Project 4 : This Website</h1>
             <p>
                 I made this website for a couple of different reasons. First, again, to teach myself the basics of UI/UX and web development.
                 It was also to make a portfolio and to tell more about myself than can be said on a resume or on LinkedIn. I used React.js to
